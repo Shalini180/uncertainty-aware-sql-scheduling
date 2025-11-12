80baa556760b46476629301f2770794cf92d3912
unknown <shalini.a.annadurai@gmail.com>
2025-11-12 09:09:01 +0530

    chore: add PowerShell tasks script (setup/run/test/fmt)

---
+++ src/utils/query_parser.py
@@ -0,0 +1,11 @@
+﻿class QueryAnalyzer:
+    def analyze(self, sql: str) -> dict:
+        return {
+            "has_join": False,
+            "has_aggregation": False,
+            "has_sort": False,
+            "has_subquery": False,
+            "tables": [],
+            "estimated_selectivity": 0.5,
+            "complexity_score": 1.0,
+        }
