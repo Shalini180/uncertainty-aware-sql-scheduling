8e4f0386aa389786c72201658aeda4a0df009cbe
unknown <shalini.a.annadurai@gmail.com>
2025-11-24 11:25:52 +0530

    Production Containerization, Gunicorn, and Persistent Volume Setup.

---
+++ gunicorn.conf.py
@@ -0,0 +1,38 @@
+"""Gunicorn configuration for production deployment"""
+
+import multiprocessing
+import os
+
+# Server socket
+bind = "0.0.0.0:8000"
+backlog = 2048
+
+# Worker processes
+workers = int(os.getenv("GUNICORN_WORKERS", "4"))
+worker_class = "uvicorn.workers.UvicornWorker"
+worker_connections = 1000
+max_requests = 1000
+max_requests_jitter = 50
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = os.getenv("LOG_LEVEL", "info")
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+
+# Process naming
+proc_name = "energy_ml_api"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL (if needed in future)
+# keyfile = None
+# certfile = None
